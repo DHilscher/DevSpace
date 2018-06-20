ffc19392f7fd2ebcec989a50f4f3aad6c7527922
System Administrator <davidhilscher@gmail.com>
2018-06-20 14:59:32 -0700

    Created JWT.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://dave:dave@ds137720.mlab.com:37720/devspace'
+  mongoURI: 'mongodb://dave:dave@ds137720.mlab.com:37720/devspace',
+  secretOrKey: 'secret'
 };
